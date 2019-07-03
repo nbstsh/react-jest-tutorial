b973a823e2dfa66f15cd7a6a9bba8e6aba19ba5c
nbstsh <nbstsh@gmail.com>
2019-07-04 03:48:17 +0900

    Implement snapshot testing

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -2,7 +2,10 @@ import { shallow, mount, render } from 'enzyme';
 import React from 'react';
 import Card from './Card';
 
-console.log(shallow(<Card />));
+// it('expect to render Card component', () => {
+// 	expect(shallow(<Card />).length).toEqual(1);
+// });
+
 it('expect to render Card component', () => {
-	expect(shallow(<Card />).length).toEqual(1);
+	expect(shallow(<Card />)).toMatchSnapshot();
 });
